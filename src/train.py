683d094284eaad920d2c866973c39d9b9a2ac9e9
Mikhail <mnrozhkov@gmail.com>
2024-02-28 12:06:51 +0100

    Solution for Checkpoints - local filesystem

--- src/train.py
+++ src/train.py
@@ -1,16 +1,17 @@
-import pandas as pd
-import joblib
-import random
-from sklearn.ensemble import RandomForestClassifier
-import time
-
-
-from dvclive import Live
-
+import argparse
+from functools import cache
+import shutil
+from fsspec.implementations.local import LocalFileSystem
 import lightning.pytorch as pl
+from lightning.pytorch.callbacks import ModelCheckpoint
+import pandas as pd
 import torch
 from torchvision.datasets import MNIST
 import torchvision.transforms as transforms
+
+from dvclive import Live
+from dvclive.lightning import DVCLiveLogger
+
 
 class LitAutoEncoder(pl.LightningModule):
     def __init__(self, encoder_size=64, lr=1e-3):
@@ -49,36 +50,69 @@ class LitAutoEncoder(pl.LightningModule):
         optimizer = torch.optim.Adam(self.parameters(), lr=self.hparams.lr)
         return optimizer
 
-def train():
+def train(resume=False):
 
     print("Training Random Forest model - START")
     
-
-
-
     transform = transforms.ToTensor()
-    train_set = MNIST(root="data/MNIST", download=True, train=True, transform=transform)
-    validation_set = MNIST(root="data/MNIST", download=True, train=False, transform=transform)
+    train_set = MNIST(root="data", download=True, train=True, transform=transform)
+    validation_set = MNIST(root="data", download=True, train=False, transform=transform)
     train_loader = torch.utils.data.DataLoader(train_set)
     validation_loader = torch.utils.data.DataLoader(validation_set)
 
-    from dvclive.lightning import DVCLiveLogger
-    from lightning.pytorch.callbacks import ModelCheckpoint
+    # 
+    model = LitAutoEncoder(encoder_size=64, lr=0.01)
+    checkpoint_callback = ModelCheckpoint(
+        dirpath='mnt/disk1/checkpoints/',
+        filename='mnist-{epoch:02d}',
+        verbose=True,
+        save_last=True,
+        save_top_k=-1,
+    )
 
-    checkpoint_callback = ModelCheckpoint(dirpath='mnt/disk1/checkpoints/')
-    
-    for encoder_size in (64, 128):
-        for lr in (1e-3, 0.1):
-            model = LitAutoEncoder(encoder_size=encoder_size, lr=lr)
-            trainer = pl.Trainer(
-                limit_train_batches=200,
-                limit_val_batches=100,
-                max_epochs=5,
-                logger=DVCLiveLogger(log_model=True, report="notebook"),
-                callbacks=[checkpoint_callback]
-            )
-            trainer.fit(model, train_loader, validation_loader)
+    # trainer = pl.Trainer(
+    #     limit_train_batches=200,
+    #     limit_val_batches=100,
+    #     max_epochs=5,
+    #     logger=DVCLiveLogger(log_model=True, save_dvc_exp=False, dvcyaml=False),
+    #     callbacks=[checkpoint_callback]
+    # )
 
+    fs = LocalFileSystem()
+    CKPT_PATH=None
+    if resume and fs.exists("mnt/disk1/checkpoints/last.ckpt"):
+        CKPT_PATH="mnt/disk1/checkpoints/last.ckpt"
+        print("Resuming from checkpoint: ", CKPT_PATH)
+
+    with Live(save_dvc_exp=False, dvcyaml=True) as live:
+
+        trainer = pl.Trainer(
+            limit_train_batches=200,
+            limit_val_batches=100,
+            max_epochs=5,
+            logger=DVCLiveLogger(log_model=False, experiment=live),
+            callbacks=[checkpoint_callback]
+        )
+        trainer.fit(
+            model, 
+            train_loader, 
+            validation_loader,
+            ckpt_path=CKPT_PATH
+        )
+
+        # Log additional metrics after training
+        model_path = checkpoint_callback.best_model_path
+        # best_model_dst = "dvclive/artifacts/best_model.ckpt"
+        best_model_dst = "models/model.ckpt"
+        shutil.copy(model_path, best_model_dst)
+        live.log_artifact(best_model_dst, name="best", type="model", copy=False)
+        
 
 if __name__ == "__main__":
-    train()
+    
+    parser = argparse.ArgumentParser(description='Train your model.')
+    parser.add_argument('--resume', action='store_true', help='Resume training if True, start new training otherwise (default: False)')
+    args = parser.parse_args()
+    
+    # Call train function with the value of resume argument
+    train(resume=args.resume)
